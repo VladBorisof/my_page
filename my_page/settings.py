8a69e0910a417337addb8627a07364b910fcca9f
VladBorisof <vl-borisof@yandex.ru>
2022-02-25 19:21:52 +0300

    ttt

--- my_page/settings.py
+++ my_page/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vc@rqk=gl3!kd1h-t&4dl$kjbd3k-gk*(_56o^3rw7s_qgd5y#'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
