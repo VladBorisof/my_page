abdb9f331021ccc703f95511faac2cd9f1734635
VladBorisof <vl-borisof@yandex.ru>
2022-03-03 22:56:46 +0300

    geometry add rectangle, square, circle

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+import numpy as np
+
 
 # Create your views here.
+
+
+def get_rectangle_area(request, width: int, height: int):
+    return HttpResponse(f"Площадь прямоугольника размером {width}х{height} равна {width * height}")
+
+
+def get_square_area(request, width: int):
+    return HttpResponse(f"<font size='5' color='red' face='Arial'> Площадь квадрата размером {width}х{width} равна {width * width} </font>")
+
+
+def get_circle_area(request, radius: int):
+    s = np.pi * radius * radius
+    return HttpResponse(f"Площадь круга с радиусом {radius} равна {s}")
