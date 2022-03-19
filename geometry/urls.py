9a613e72e2a5e040a6e39add63669eb9f82b2a97
VladBorisof <vl-borisof@yandex.ru>
2022-03-19 12:32:37 +0300

    reverse function for calculate_geometry

--- geometry/urls.py
+++ geometry/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rectangle/<int:width>/<int:height>/', views.get_rectangle_area),
-    path('square/<int:width>/', views.get_square_area),
-    path('circle/<int:radius>/', views.get_circle_area),
+    path('rectangle/<int:width>/<int:height>/', views.get_rectangle_area, name='rectangle-name'),
+    path('square/<int:width>/', views.get_square_area, name='square-name'),
+    path('circle/<int:radius>/', views.get_circle_area, name='circle-name'),
+
     path('get_rectangle_area/<int:width>/<int:height>/', views.rectangle),
     path('get_square_area/<int:width>/', views.square),
     path('get_circle_area/<int:radius>/', views.circle),
