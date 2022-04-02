3d63ad88316b0d05b14fdd20c5ec026b3a578d5b
VladBorisof <vl-borisof@yandex.ru>
2022-04-02 21:53:33 +0300

    add types for horoscope

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('tipes/', views.get_view_by_tipes),
+    path('tipes/<str:tipe>', views.get_info_by_tipes, name='horoscope-type'),
     path('<int:sign_zodiac>/', views.get_info_zodiac_number),
     path('<str:sign_zodiac>/', views.get_info_zodiac, name='horoscope-name'),
 
